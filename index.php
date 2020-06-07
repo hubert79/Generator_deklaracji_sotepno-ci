344e9ab1a8d697cc66727c617cf3af98af69db63
hubert79 <makula.hubert@gmail.com>
2020-06-08 00:18:19 +0200

    good version

--- index.php
+++ index.php
@@ -1,87 +1,86 @@
-    <!DOCTYPE html>
-    <html lang="pl">
-    <head>
-
-        <meta charset="utf-8"/>
-        <title>test</title>
-        <link rel="stylesheet" href="css/style.css">
-		
-    </head>
-        <body>
-        <div id="container">
-        <h1>Generator dostępności</h1>
-
-
-        <form action="generate.php" method="post">
+<!DOCTYPE html>
+<html lang="pl">
+	<head>
+		<meta charset="utf-8"/>
+		<title>test</title>
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+		<div id="container">
+			<h1>Generator dostępności</h1>
 			
-			<div class="row">
-			<fieldset>
-				<div><label>Nazwa podmiotu: <input type="text" name="entityName" /></label></div>
-				<div><label>Adres URL podmiotu: <input type="text" name="entityURLAdress" /></label></div>
-				<div><label>Data publikacji strony: <input type="text" name="dateOfPublication" /></label></div>
-				<div><label>Data ostatniej istotnej aktualizacji: <input type="text" name="dateOfLastUpdate" /></label></div>
-			</fieldset>
-			</div>
-			<div class="row">
-			<fieldset>
-				<label for="status">Status pod względem zgodności z ustawą</label>
-				<select id="status" name="status">
-                     <option onclick="statusDropDown('contentNotAccessible','labcontentNotAccessible', 
-					 'off', 'laboff', 'link', 'lablink', true);">Zgodna</option>
-                     <option onclick="statusDropDown('contentNotAccessible','labcontentNotAccessible', 
-					 'off', 'laboff' ,'link', 'lablink' ,false);">Częściowo zgodna</option>
-                     <option onclick="statusDropDown('contentNotAccessible','labcontentNotAccessible', 
-					 'off', 'laboff', 'link', 'lablink' , false);">Niezgodna</option>
-                </select>
+			<form action="generate.php" method="post">
 				
-				<div id="labcontentNotAccessible"></div>
-				<div id="contentNotAccessible"></div>
-				<div id="laboff"></div>
-                <div id="off"></div>
-				<div id="lablink"></div>
-                <div id="link"></div>
-			</fieldset>
-			</div>
-			<div class="row">
-			<fieldset>
-				<label for="declaration">Deklaracje sporządzona została na podstawie:</label>
-				<select id="declaration" name="declarationMade">
-                    <option onclick="extermalEntity('nameExtermalEntity', 'nameExtermalEntityLabel', true);">samooceny przeprowadzonej przez podmiot publiczny.</option>
-                    <option onclick="extermalEntity('nameExtermalEntity', 'nameExtermalEntityLabel',  false);">badania przeprowadzonego przez podmiot zewnętrzny: </option>
-                </select>
-				<div id="nameExtermalEntityLabel"></dic>
-				<div id="nameExtermalEntity"></div>
-			</fieldset>
-			</div>
-			<div class="row">
-            <fieldset>
-				<label>Dostępność architektoniczna</label>
-				<textarea></textarea>
-			</fieldset>
-			</div>
-			<div class="row">
-			<fieldset>
-				<label for="mobapp">Aplikacja mobilna:</label>
-				<select id="mobapp"">
-                   <option onclick="mobileAppp('dma', 'uma', 'labeldma', 'labeluma',  'bma', true);">Tak</option>
-                   <option onclick="mobileAppp('dma', 'uma', 'labeldma', 'labeluma', 'bma', false);">Nie</option>
-					
-               </select>
-				<div id="labeldma"></div>
-				<div id="dma"></div>
-				<div id="labeluma"></div>
-				<div id="uma"></div>
-				<div id="bma"></div>
-			</fieldset>
-			</div>
-
-       </form>
-    </div>
-</body>
-
-    <script type="text/javascript" src="js/valid.js"></script>
-	<script type="text/javascript" src="js/extendForm.js"></script>
+				<div class="row">
+					<fieldset>
+						<div><label>Nazwa podmiotu: <input type="text" name="entityName" /></label></div>
+						<div><label>Adres URL podmiotu: <input type="url" name="entityURLAdress"/></label></div>
+						
+						<label for="dateOfPublication">Data publikacji strony: </label>
+						<select name="dateOfPublication" id="dateOfPublication">
+							
+						</select>
+						
+						<div><label>Data ostatniej istotnej aktualizacji: <input type="date" name="dateOfLastUpdate" /></label></div>
+					</fieldset>
+				</div>
+				<div class="row">
+					<fieldset>
+						<label for="selectStatus">Status pod względem zgodności</label>
+						<select name="selectStatus" id="selectStatus">
+							<option value="Zgodna">Zgodna</option>
+							<option value="Częściowo zgodna">Częściowo zgodna</option>
+							<option value="Niezgodna">Niezgodna</option>
+						</select>
+						<div id="addStatusInput" class="status-is-hidden">
+							<div><label>Treść niedostępna: <input type="text" id="contentNotAccessibleStatus" name="contentNotAccessible" /></label></div>
+							<div><label>Wyłączenia: <input type="text" id="offStatus" name="offStatus" /></label></div>
+							<div><label>Link: <input type="url" id="linkStatus" name="linkStatus" /></label></div>
+						</div>
+					</fieldset>
+				</div>
+				<div class="row">
+					<fieldset>
+						<label for="declaration">Deklaracje sporządzona została na podstawie:</label>
+						<select id="declaration" name="declarationMade">
+							<option value="Samooceny przeprowadzonej przez podmiot publiczny">Samooceny przeprowadzonej przez podmiot publiczny</option>
+							<option value="Badania przeprowadzonego przez podmiot zewnętrzny">Badania przeprowadzonego przez podmiot zewnętrzny</option>
+						</select>
+						<div id="addDeclarationInput" class="declaration-is-hidden">
+							<div><label>Nazwa podmiotu zewnętrznego: <input type="text" id="nameExtermalEntity" name="nameExtermalEntity" /></label></div>
+						</div>
+					</fieldset>
+				</div>
+				<div class="row">
+					<fieldset>
+						<label for="archaccess">Dostępność architektoniczna</label>
+						<textarea id="archaccess" name="archaccess"></textarea>
+					</fieldset>
+				</div>
+				<div class="row">
+					<fieldset>
+						<label for="mobApp">Aplikacja mobilna:</label>
+						<select id="mobApp" name="mobApp">
+							<option value="Tak">Tak</option>
+							<option value="Nie">Nie</option>
+						</select>
+						<div id="addMobileAppInput" class="mobileApp-is-hidden">
+							<div><label>Opis: <input type="text" id="describeMobileApp" name="describeMobileApp" /></label></div>
+							<div><label>Link: <input type="url" id="linkMobileApp" name="linkMobileApp" /></label></div>
+						</div>
+					</fieldset>
+				</div>
+				
+				<input type="submit" value="Generuj"/>
+			</form>
+		</div>
+	</body>
+	<!-- Validators -->
+	<script type="text/javascript" src="js/valid.js"></script>
+	
+	
+	<script type="text/javascript" src="js/status.js"></script>
+	<script type="text/javascript" src="js/declaration.js"></script>
 	<script type="text/javascript" src="js/mobileApp.js"></script>
-	<script type="text/javascript" src="js/kolejny.js"></script>
-    </html>
 
+</html>
--- js/mobileApp.js
+++ js/mobileApp.js
@@ -1,71 +1,10 @@
-//mobileApp
 
-var ma = 0;
-	function mobileAppp(dis, url, dislab, urllab,buton,  wybor){
-		
-		if(wybor == true){
-		if (ma == 0){
-			
-		var ol = document.createElement('LABEL');
-			//u.className = 'upload';
-			ol.setAttribute('for', 'dpisapp');
-			ol.setAttribute("id", "olid");
-			ol.innerHTML = "Opis aplikacji mobilnej";
-			var kontener = document.getElementById(dislab);
-			kontener.appendChild(ol);	
-			
-		var o = document.createElement('input');
-			o.setAttribute('type', 'text');
-			o.setAttribute('id', 'dpisapp');
-			o.setAttribute('name', 'plik[]');
-			o.className = 'upload';
-			var kontener = document.getElementById(dis);
-			kontener.appendChild(o);
-			
-		var ul = document.createElement('LABEL');
-			//u.className = 'upload';
-			ul.setAttribute('for', 'arlmapp');
-			ul.setAttribute("id", "ulid");
-			ul.innerHTML = "Link aplikacji mobilnej";
-			var kontener = document.getElementById(urllab);
-			kontener.appendChild(ul);	
-			
-		var u = document.createElement('input');
-			u.setAttribute('type', 'text');
-			u.setAttribute('id', 'urlmapp');
-			u.setAttribute('name', 'plik[]');
-			u.className = 'upload';
-			var kontener = document.getElementById(url);
-			kontener.appendChild(u);
-			
-			
-		var dodaj = document.createElement('input');
-			dodaj.setAttribute('type', 'button');
-			dodaj.setAttribute('id', 'but');
-			dodaj.setAttribute('name', 'plik[]');
-			dodaj.setAttribute('onclick', 'kolejny();');
-			dodaj.className = 'upload';
-			var kontener = document.getElementById(buton);
-			kontener.appendChild(dodaj);
-			
-			ma = 1;
-			}
-		}
-		else if(wybor == false){
-			if( ma == 1){
-				var e1 = document.getElementById('dpisapp');
-				var e2 = document.getElementById('urlmapp');
-				var e3 = document.getElementById('but');
-				var e4 = document.getElementById('olid');
-				var e5 = document.getElementById('ulid');
-				
-				e1.remove();
-				e2.remove();
-				e3.remove();
-				e4.remove();
-				e5.remove();
-				ma = 0;
-			}
-		}
 
-	}
+const selectMobileApp = document.getElementById('mobApp');
+const addInputsMobileApp = document.getElementById('addMobileAppInput');
+
+	selectMobileApp.addEventListener('change', e => {
+		e.target.value == 'Tak' ?
+		addInputsMobileApp.classList.remove('mobileApp-is-hidden')	:
+		addInputsMobileApp.classList.add('mobileApp-is-hidden')
+	});
