87a1b19c95636229d80d6adcf7f980ab4c498f07
hubert79 <makula.hubert@gmail.com>
2020-06-08 22:34:56 +0200

    new version with dynamic form

--- generate.php
+++ generate.php
@@ -8,52 +8,63 @@
     <body>
     
     <?php
+	
 	$entityName = $_POST['entityName'];
 	$entityURLAdress = $_POST['entityURLAdress'];
-	$dateOfPublication = $_POST['dateOfPublication'];
-	$dateOfLastUpdate = $_POST['dateOfLastUpdate'];
-	
+	$yearDateOfPublication = $_POST['yearDateOfPublication'];
+	$monthDateOfPublication = $_POST['monthDateOfPublication'];
+	$dayDateOfPublication = $_POST['dayDateOfPublication'];
+	$yearDateOfLastUpdate = $_POST['yearDateOfLastUpdate'];
+	$monthDateOfLastUpdate = $_POST['monthDateOfLastUpdate'];
+	$dayDateOfLastUpdate = $_POST['dayDateOfLastUpdate'];
 	$selectStatus = $_POST['selectStatus'];
-	$contentNotAccessible = $_POST['contentNotAccessible'];
-	$offStatus = $_POST['offStatus'];
-	$linkStatus = $_POST['linkStatus'];
-	
 	$declarationMade = $_POST['declarationMade'];
-	$nameExtermalEntity = $_POST['nameExtermalEntity'];
-	
 	$archaccess = $_POST['archaccess'];
-	
 	$mobApp = $_POST['mobApp'];
-	$describeMobileApp = $_POST['describeMobileApp'];
-	$linkMobileApp = $_POST['linkMobileApp'];
+	
 
 	echo "$entityName, nazwa podmiotu <br>";
-	echo "$entityURLAdress, URL podmiotu <br>";
-	echo "$dateOfPublication, Data publikacji <br>";
-	echo "$dateOfLastUpdate, Data aktualizacji <br>";
-	
+	echo "$entityURLAdress, Adres URL podmiotu <br>";
+	echo "$yearDateOfPublication, rok <br>";
+	echo "$monthDateOfPublication, Miesiąc <br>";
+	echo "$dayDateOfPublication, dzień <br>";
+	echo "$yearDateOfLastUpdate, rok <br>";
+	echo "$monthDateOfLastUpdate, miesiąc <br>";
+	echo "$dayDateOfLastUpdate, dzień <br>";
 	echo "$selectStatus, Status <br>";
-	echo "$contentNotAccessible, Treść niedostępna <br>";
-	echo "$offStatus, Wyłączenia <br>";
-	echo "$linkStatus, Link <br>";
 	
-	echo "$declarationMade, Deklaracja <br>";
-	echo "$nameExtermalEntity, nazwa podmiotu zewnętrznego <br>";
+	if(strlen($selectStatus) > 6){
+		$contentNotAccessible = $_POST['contentNotAccessible'];
+		$offStatus = $_POST['offStatus'];
+		$linkStatus = $_POST['linkStatus'];
+		
+		echo "$contentNotAccessible, treść niedostępna <br>";
+		echo "$offStatus, wyłączenia <br>";
+		echo "$linkStatus, linki wyłączeń <br>";
+	}
 	
-	echo "$archaccess, Dostępność architektoniczna <br>";
+	echo "$declarationMade, deklaracja <br>";
 	
+	if(strlen($declarationMade) > 49)
+	{
+		$nameExtermalEntity = $_POST['nameExtermalEntity'];
+		
+		echo "$nameExtermalEntity, nazwa podmiotu zewnętrznego <br>";
+	}
+	
+	echo "$archaccess, archtiektura <br>";
 	echo "$mobApp, Aplikacja mobilna <br>";
-	echo "$describeMobileApp, Opis am <br>";
-	echo "$linkMobileApp, Link am <br>";
 	
-
+	if(strstr($mobApp, "Tak") == true)
+	{
+		$describeMobileApp = $_POST['describeMobileApp'];
+		$linkMobileApp = $_POST['linkMobileApp'];
+		
+		echo "$describeMobileApp, opis <br>";
+		echo "$linkMobileApp, link <br>";
+		
+	}
+	
     ?>
     </body>
 </html>
-
-    
-    
-    
-  
-            
- 
--- js/mobileApp.js
+++ js/mobileApp.js
@@ -4,7 +4,8 @@ const selectMobileApp = document.getElementById('mobApp');
 const addInputsMobileApp = document.getElementById('addMobileAppInput');
 
 	selectMobileApp.addEventListener('change', e => {
-		e.target.value == 'Tak' ?
-		addInputsMobileApp.classList.remove('mobileApp-is-hidden')	:
-		addInputsMobileApp.classList.add('mobileApp-is-hidden')
+		e.target.value == 'Nie' ?
+		addInputsMobileApp.classList.add('mobileApp-is-hidden')	:
+		addInputsMobileApp.classList.remove('mobileApp-is-hidden')
+		
 	});
